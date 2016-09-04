cd35be9a3842608dfbae7e12511ffea9c2e89bed
P.N.Vamshi <pnvamshi9@gmail.com>
2016-09-05 00:08:25 +0530

    Add files via upload

---
+++ AES-128-Bit-Verilog/AES_TOP.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:14:24 07/15/2015 
+// Design Name: 
+// Module Name:    AES_TOP 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module AES_TOP(clk,finalout);
+    input clk;
+    output [7:0] finalout;
+
+wire [127:0] tempout;
+
+aescipher u1(.clk(clk),.datain(128'h 3243f6a8885a308d313198a2e0370734),.key(128'h 2b7e151628aed2a6abf7158809cf4f3c),.dataout(tempout));
+
+assign finalout = tempout[127:120];
+
+endmodule
