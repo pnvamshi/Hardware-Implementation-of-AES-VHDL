b5609ffbf72ceed0f2f5bc6b05178b45b9709e85
P.N.Vamshi <pnvamshi9@gmail.com>
2016-09-05 00:11:44 +0530

    Update AES_TB.v

--- AES-128-Bit-Verilog/AES_TB.v
+++ AES-128-Bit-Verilog/AES_TB.v
@@ -1,27 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   00:19:18 07/15/2015
-// Design Name:   AES_TOP
-// Module Name:   C:/Users/Shourya/Documents/Xilinx Projects/AES_Verilog/AES_TB.v
-// Project Name:  AES_Verilog
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: AES_TOP
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
-
 module AES_TB;
 
 	// Inputs
